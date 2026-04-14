b020d15afe8c2ac73c60a33612b746b8af19a3ee
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-14 09:50:10 +0400

    Task 15: Store and Manage Unique Product Names in a Java Set  completed

---
+++ src/ProductSetDemo1.java
@@ -0,0 +1,52 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ProductSetDemo1 {
+    public static void main(String[] args) {
+        Set<String> productNames = new HashSet<>();
+
+        //add 5 element
+        productNames.add("iPhone 15");
+        productNames.add("MacBook ");
+        productNames.add("AirPods");
+        productNames.add("iPad Air");
+        productNames.add("Apple Watch");
+
+        //Display all element
+        System.out.println("Initial products: " + productNames);
+
+        //Try to add duplicate element
+        productNames.add("iPhone 15");
+
+        //Display after duplicate
+        System.out.println("After duplicate : " + productNames);
+
+        /*==================
+        contains(): check if product exists
+        ==================*/
+        System.out.println("iPhone 15 exists: " + productNames.contains("iPhone 15"));
+        System.out.println("z.com exists: " + productNames.contains("Samsung Galaxy"));
+
+        //remove one element  name
+        productNames.remove("MacBook Pro");
+
+        //Display after remove
+        System.out.println("After remove MacBook Pro: " + productNames);
+
+        /*==================
+        size(): total element count
+        ==================*/
+        System.out.println("Total products: " + productNames.size());
+
+        //clear all element
+        productNames.clear();
+
+        //Display after clear
+        System.out.println("After clear: " + productNames);
+
+        /*=================
+        clear(): remove all elements
+        ==================*/
+        System.out.println("Final size: " + productNames.size());
+    }
+}
